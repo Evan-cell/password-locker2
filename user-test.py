6c871936b8c3690e0d8bae0225595349aa53aff8
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-04 18:26:42 +0300

    created a setupto test

--- user-test.py
+++ user-test.py
@@ -9,3 +9,15 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = User("Evan","kimani","0793511825","Evan@ms.com") # create contact object
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()    
