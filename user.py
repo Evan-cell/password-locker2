a61ba84a13315954e9141367291c9d206cfc8d96
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-04 18:10:50 +0300

    added save function

--- user.py
+++ user.py
@@ -4,3 +4,13 @@ class user:
     
     '''
     user_list = []
+    
+    def __init__(self,user_name, password):
+        self.user_name = user_name
+        self.password = password
+    def save_user(self):
+        '''
+        saves account details of the user information
+        
+        '''    
+        
