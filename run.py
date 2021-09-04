fcc9cdbaa55164ab31145b9934bc6d5667b7f2fc
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-04 23:14:00 +0300

    created a save user info

--- run.py
+++ run.py
@@ -3,7 +3,12 @@ from user import user
 
 def create_user(user_name, password):
     '''
-    Function to create a new contact
+    Function to create a new user
     '''
     new_user = user(user_name, password)
     return new_user
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
