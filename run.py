900f41f09245dba72f0d14a01dcf0b0a0cc7da11
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-04 23:09:40 +0300

    added create contact method

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 import random
+from user import user
+
+def create_user(user_name, password):
+    '''
+    Function to create a new contact
+    '''
+    new_user = user(user_name, password)
+    return new_user
